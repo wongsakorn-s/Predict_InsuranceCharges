0c2dc74bcfd871f7809a2bf5366e6b2c02aa5f2a
Wongsakorn Saengsurasak <68773278+wongsakorn-s@users.noreply.github.com>
2023-09-30 15:46:21 +0700

    Update insurance_app.py

--- insurance_app.py
+++ insurance_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import joblib
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 # Load the saved model
 loaded_model = joblib.load('linear_regression_model.pkl')
@@ -24,3 +26,13 @@ predicted_charges = loaded_model.predict(example_data)
 
 # Display the prediction
 st.write(f"Predicted Charges: ${predicted_charges[0]:.2f}")
+
+# Add calculation of Mean Squared Error (MSE) and Root Mean Squared Error (RMSE)
+actual_charges = 10000  # Replace with the actual charges if available
+if actual_charges:
+    mse = mean_squared_error([actual_charges], [predicted_charges[0]])
+    rmse = np.sqrt(mse)
+    st.write(f"MSE: {mse:.2f}")
+    st.write(f"RMSE: {rmse:.2f}")
+else:
+    st.write("Actual charges not provided. Unable to calculate MSE and RMSE.")
